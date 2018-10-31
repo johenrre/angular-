ab1ae782c17f5111862bac7b56b146e36e71d505
rocking <494057020@qq.com>
2018-10-31 15:55:38 +0800

    删除多余代码

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,47 +9,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgZorroAntdModule, NZ_I18N, zh_CN } from 'ng-zorro-antd';
 import { registerLocaleData } from '@angular/common';
 import zh from '@angular/common/locales/zh';
-import { RegisterComponent } from './register/register.component';
-import { InputComponent } from './input/input.component';
-
-import { AppRoutingModule } from './app-routing/app-routing.module';
-import { GridComponent } from './grid/grid.component';
-import { NumInputComponent } from './num-input/num-input.component';
-import { InputValidateComponent } from './input-validate/input-validate.component';
-import { TimePickerComponent } from './time-picker/time-picker.component';
-import { ModalComponent } from './modal/modal.component';
-import { UniqueAlterEgoValidatorDirective } from './input/alter-ego.directive';
-import { ChartsComponent } from './charts/charts.component';
-import { LineChartModule } from '@swimlane/ngx-charts';
-import { NgxDatatableModule } from '@swimlane/ngx-datatable';
-
-
-import { ChartsModule } from './charts/charts.module';
-
-import { NgxEchartsModule } from 'ngx-echarts';
-import { DirectiveTestComponent } from './directive-test/directive-test.component';
-import { GreetDirective } from './directive-test/geek.directive';
-import { TableComponent } from './table/table.component';
-import { LayoutComponent } from './layout/layout.component';
 
 registerLocaleData(zh);
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent,
-    InputComponent,
-    GridComponent,
-    NumInputComponent,
-    InputValidateComponent,
-    TimePickerComponent,
-    ModalComponent,
-    UniqueAlterEgoValidatorDirective,
-    ChartsComponent,
-    DirectiveTestComponent,
-    GreetDirective,
-    TableComponent,
-    LayoutComponent,
   ],
   imports: [
     BrowserModule,
@@ -58,11 +23,6 @@ registerLocaleData(zh);
     HttpClientModule,
     NgZorroAntdModule,
     ReactiveFormsModule,
-    AppRoutingModule,
-    NgxEchartsModule,
-    ChartsModule,
-    LineChartModule,
-    NgxDatatableModule,
   ],
   providers: [{ provide: NZ_I18N, useValue: zh_CN }],
   bootstrap: [AppComponent]
--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,12 +1,4 @@
-import { Component, OnInit, OnChanges } from '@angular/core';
-import { AsyncValidator, AbstractControl, ValidationErrors, FormBuilder } from '@angular/forms';
-import { Observable } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
-
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { UniqueAlterEgoValidator } from './alter-ego.directive';
-
-const { required, maxLength, minLength, max, min } = Validators;
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-input',
@@ -15,25 +7,9 @@ const { required, maxLength, minLength, max, min } = Validators;
 })
 export class InputComponent implements OnInit {
 
-  heroForm: FormGroup;
-  get status() {
-    return this.alterEgo.pending ? 'validating' : null;
+  constructor() { }
+
+  ngOnInit() {
   }
 
-  ngOnInit(): void {
-    this.heroForm = this.fb.group({
-      'alterEgo': new FormControl(null, {
-        validators: [required, maxLength(20)],
-        asyncValidators: [this.alterEgoValidator.validate.bind(this.alterEgoValidator)],
-        updateOn: 'blur',
-      }),
-    });
-
-  }
-
-  get alterEgo() { return this.heroForm.get('alterEgo'); }
-
-  constructor(
-    private readonly fb: FormBuilder,
-    private alterEgoValidator: UniqueAlterEgoValidator) { }
 }
